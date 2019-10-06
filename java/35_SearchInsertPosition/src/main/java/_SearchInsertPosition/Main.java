69d34be00754fb2d0ae559495288e0a4e8a3d6a7
wei170123 <sanirvana8105@gmail.com>
2019-10-06 18:17:33 +0800

    35. Search Insert Position

---
+++ java/35_SearchInsertPosition/src/main/java/_SearchInsertPosition/Main.java
@@ -0,0 +1,18 @@
+package _SearchInsertPosition;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] nums = {1,3,5,6};
+		int target = 5;
+		
+		int i = 0;
+		for(; i < nums.length; i++) {
+			if(target <= nums[i]) {
+//				return i;
+			}
+		}
+//		return i;
+	}
+
+}
